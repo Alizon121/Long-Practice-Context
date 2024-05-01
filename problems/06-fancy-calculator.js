7ec542c26d279b4fe030111680e3b66cfe6be27a
Andrew <andrew.lizon12@gmail.com>
2024-04-30 20:34:21 -0700

    Completed Phases 5-6 Driver:Andrew/Nav:Etoria

--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,4 +1,26 @@
-// Your code here 
+const Calculator = require("./02-calculator.js")
+
+class FancyCalculator extends Calculator {
+constructor(total = 0) {
+	super(total = 0)
+}
+setTotal(newTotal) {
+	this.total = newTotal;
+	return this.total;
+}
+modulo(num){
+	if (num === 0) {
+		throw new error ("Cannot divide by zero");
+	}
+	this.total %= num ;
+	return this.total;
+}
+squared() {
+	this.total = this.total ** 2;
+	return this.total;
+}
+
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
